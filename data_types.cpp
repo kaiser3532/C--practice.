953778fc98497c2b83fa6dc1cb0fddc45d129ce8
kaiser3532 <kaisermalik3532@gmail.com>
2026-01-22 07:22:02 +0000

    Create data_types.cpp

---
+++ data_types.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int x;
+    long long y;
+    char a;
+    float z;
+    double t;
+    cin>>x >>y >>a >>z >>t;
+    cout<<x<<endl;
+    cout<<y<<endl;
+    cout<<a<<endl;
+    cout<<fixed<<setprecision(3) <<z<<endl;
+    cout<<fixed<<setprecision(9)<<t<<endl;
+    
+
+        return 0;
+}
