733cc0e29736260899cd8a27d3b4a54837ee4906
kaiser3532 <kaisermalik3532@gmail.com>
2026-01-25 10:13:40 +0000

    Create literals.cpp

---
+++ literals.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+usingnamespace std;
+//int z=23;//global variable
+int main(){
+
+// float d=34.4;
+// long double e=34.4l;  
+// //by default c++ compiler tells us 34.4 is double when it should be float but this is not true '
+//  cout<<"The size of 34.4 "<<sizeof(34.4)<<endl;// double size as compiler takes it by default 
+//   cout<<"The size of 34.4f "<<sizeof(34.4f)<<endl;
+//    cout<<"The size of 34.4F "<<sizeof(34.4F)<<endl;
+//     cout<<"The size of 34.4l "<<sizeof(34.4l)<<endl;
+//      cout<<"The size of 34.4L "<<sizeof(34.4L)<<endl;
+// cout<<"The value of d is "<<d<<endl<<"The value of e is "<<e<<endl;
+return 0;
+}
