c59cba8a3c7b84be31965912e475be55ca8a66f8
kaiser3532 <kaisermalik3532@gmail.com>
2026-01-25 10:10:25 +0000

    Create  variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+// int z=23;//global variable
+int main(){
+//  int x;
+//  int y;
+//     cout<<"Enter the value of x"<<endl;
+//     cin>>x;
+//     cout<<"Enter the value of y"<<endl;
+//     cin>>y;
+//     int z=x+y;
+    
+//     cout<<"Sum is:"<<z<<endl;
+//     cout<<"Global variable z:"<<::z<<endl;//global variable is extracted by using scope resulation operator..
+return 0;
+}
