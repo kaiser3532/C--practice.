8cc47d1362352cb4879ef851336f8461d9212b7e
kaiser3532 <kaisermalik3532@gmail.com>
2026-01-25 10:16:39 +0000

    Create reference_variables.cpp

---
+++ reference_variables.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+//**practice_1*** */
+// int x=23;
+// int &y=x;//We use reference variables in C++ to work directly with the original data without copying it.
+// y=34;
+// cout<<x<<endl;
+// cout<<y;
+//**practice_2*** */
+// int a;
+// // int &b;//this is not valid..
+// int &b=a;
+// a= 10;
+// // a= 10 + &b;//Number + address is invalid
+// a = 10 + b;
+// b = 20 - b;
+// cout<<a<<endl;
+return 0;
+}
