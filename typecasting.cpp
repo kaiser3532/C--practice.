b6a2ae2411488f062c2e55369c184c547857386c
kaiser3532 <kaisermalik3532@gmail.com>
2026-01-25 10:23:03 +0000

    Create typecasting.cpp

---
+++ typecasting.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+// int a = 5;
+// int b = 2;
+// float result = (float)a / b;  // 2.5
+// cout<<result;
+
+//***practice_1*** */
+// int a=5;
+// float b=(float)a;
+// cout<<a<<endl;
+// cout<<b;
+
+//***practice_2*** */
+// float x = 9.8;
+// //  x=int(9.8);// THIS WORKS BUT YOU ARE STORING INT VALUE INTO FLOAT VARIABLE
+// int y= int(x);
+//  cout<<y;
+
+//***practice_3*** */
+// char ch='A';
+// int value=(int)ch;//char to integer.
+// cout<<value<<endl;
+
+//***practice_4*** */
+// int a=7, b=2;
+// //int result= (float)a/b;// this will not store it float because container is still int type 
+// float result= (float)a/b;
+// cout<<result;
+
+    return 0;
+}
