8b559e3fb03efddb375cc6af44d4049910122689
kaiser3532 <kaisermalik3532@gmail.com>
2026-01-28 04:17:43 +0000

    Create constants.cpp

---
+++ constants.cpp
@@ -0,0 +1,9 @@
+//To prevent accidental modification
+
+//To make code safer and more readable
+
+//To represent fixed values (like π, limits, configuration values)
+const int a=45;
+cout<<a;
+a=78;//error shows you can not change value of integer a.because a is constant.
+cout<<a;
